86de31bb42de5337a8bc6ec2cfdd7b11c016e34e
Jordan Hatch <jordan@jordanh.net>
2015-07-09 09:58:10 +0100

    Fix giving awards to projects with strong params

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -37,7 +37,8 @@ private
   def project_params
     params.fetch(:project, {}).permit(:title, :team, :url, :secret, :image,
       :summary, :description, :ideas, :data, :twitter, :github_url, :svn_url,
-      :code_url, :awards_attributes, :centre, :centre_id, :slug)
+      :code_url, :awards_attributes, :centre, :centre_id, :slug,
+      awards_attributes: [:id, :award_category_id, :_destroy])
   end
 
 end
