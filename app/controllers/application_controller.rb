1d50a06d6dcdd96949a2750b60c76c881b9335ef
Jordan Hatch <jordan@jordanh.net>
2015-07-09 09:58:16 +0100

    Remove the events breadcrumb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :events_breadcrumb
-
   def not_found
     raise ActionController::RoutingError.new('Not Found')
-  end
-
-  def events_breadcrumb
-    # breadcrumbs.add "Events", "http://rewiredstate.org/events"
-    breadcrumbs.add "Hacks", root_path
   end
 
   private
